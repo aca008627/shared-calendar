ff2dd6baba80e87ad7a41f0af4a8ec5973336a52
aca008627 <aca008627@gmail.com>
2026-09-12 10:55:11 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = "shared-calendar-v14-date-jump-fixed";
+const CACHE = "shared-calendar-v15-original-header-layout";
 const ASSETS = ["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./icons/icon-180.png","./icons/icon-192.png","./icons/icon-512.png","./icons/characters/dad.png","./icons/characters/mom.png","./icons/characters/boy.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
